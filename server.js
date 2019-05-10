5da3b2168dad1a6e6d9806c3fddc80a465a5c3f7
davidjrada <davidjrada@gmail.com>
2019-05-10 15:13:01 -0400

    Created basic Server on localHost

---
+++ server.js
@@ -0,0 +1,47 @@
+//Dependencies
+const express = require('express')
+const app = express()
+const PORT = 3004
+const cors = require('cors')
+const mongoose = require('mongoose')
+require('dotenv').config()
+const MONGODB_URI = 'mongodb://localhost:27017'+ '/gitpub'
+
+// const whitelist = ['http://localhost:3000', 'https://fathomless-sierra-68956.herokuapp.com']
+// const corsOptions = {
+//   origin (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
+
+//MiddleWare
+app.use(express.json()) // use .json(), not .urlencoded()
+// app.use(cors(corsOptions))
+
+// Error / Disconnection
+mongoose.connection.on('error', err => console.log(err.message + ' is Mongod not running?'))
+mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
+
+// Fix depreciation warnings
+mongoose.set('useFindAndModify', false)
+
+// Database connection
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
+mongoose.connection.once('open', () => {
+  console.log('connected to mongoose...')
+})
+
+
+app.get('/', (req, res) => {
+    res.send('GitPub')
+  })
+  
+  // Listen
+  app.listen(PORT, () => {
+    console.log('🎉🎊', 'celebrations happening on port', PORT, '🎉🎊')
+  })
+  
